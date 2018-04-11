56528c38571b51da0123e381b540f9f8fa3d0df1
Scott Pierce <scott.pierce@centro.net>
2018-04-11 08:30:16 -0500

    Fix zooming

--- README.md
+++ README.md
@@ -7,20 +7,18 @@
 crystal src/ansi.cr
 
 # another place
-crystal src/ansi.cr -l -0.7487667169208654 -r -0.7487666890984621 -t 0.12364085859010267 -b 0.12364084306850771
+crystal src/ansi.cr -- -x 0.7288780059956085 -y -0.29203160173167625 -z 2.0e-13 --max 200
 
 # yet another place, with more detail
-crystal src/ansi.cr -l -0.10731654060226994 -r -0.106766205630802 -t -0.9124674709650323 -b -0.913017793635695 --max 179
+crystal src/ansi.cr -- -x 0.7346674678648881 -y 0.20688503152309215 -z 2.0e-13 --max 275
 ```
 
 ## Usage
 
 ```
-Usage: mandelbrot [arguments]
-    -l LEFT, --left=LEFT             initial left edge
-    -r RIGHT, --right=RIGHT          initial right edge
-    -t TOP, --top=TOP                initial top edge
-    -b BOTTOM, --bottom=BOTTOM       initial bottom edge
+Usage: crystal src/ansi.cr -- [arguments]
+    -x X                             center point X
+    -y Y                             center point Y
     -z ZOOM, --zoom=ZOOM             initial zoom
     --max MAX, --max=MAX             max iterations
     --fps=FPS                        max FPS
--- src/ansi.cr
+++ src/ansi.cr
@@ -4,7 +4,8 @@ require "option_parser"
 Signal::INT.trap { exit 1 }
 Signal::TERM.trap { exit 1 }
 
-ANSI_MAP = %w(@ 0 o % X x o * " ' .)
+ANSI_MAP = %w(@ 0 # % X x o * + - . . .)
+MIN_ZOOM = 2.0e-13
 
 # Formula from https://www.mathworks.com/help/distcomp/examples/illustrating-three-approaches-to-gpu-computing-the-mandelbrot-set.html?s_tid=gn_loc_drop#d119e4796
 def mandelbrot(real0, img0, max)
@@ -41,39 +42,53 @@ def render(left, right, top, bottom, step_x, step_y, max) String
   result[0, result.size - 1].join
 end
 
-def zoomer(left, right, top, bottom, max : Int32) String
+def zoomer(mid_x, mid_y, zoom, max : Int32) String
   print "\e[?47h"     # use alternate terminal screen output
   input = ' '
-  left0, right0, top0, bottom0, max0 = left, right, top, bottom, max
+
+  mid_x0, mid_y0, zoom0, max0 = mid_x, mid_y, zoom, max
   while input != 'q'
     size            = `stty size`.split
-    x, y            = size[1].to_f - 1, size[0].to_f - 2
+    view_width, view_height            = size[1].to_f - 1, size[0].to_f - 2
+    # scaled y coordinate of pixel (must be scaled to lie somewhere in the mandelbrot Y scale (-1, 1)
+    scale_h = view_height * 0.5 / view_height * 2.0 * zoom
+    scale_w =  view_width * 0.5 /  view_width * 3.5 * zoom
 
-    step_x = (right - left) / x
-    step_y = (bottom - top) / y
+    top    = -scale_h - mid_y
+    bottom =  scale_h - mid_y
+    left   = -scale_w - mid_x
+    right  =  scale_w - mid_x
+
+    step_x = (right - left) / view_width
+    step_y = (bottom - top) / view_height
+
+    zoom = [MIN_ZOOM, zoom].max
 
     print render(left: left, right: right, top: top, bottom: bottom, step_x: step_x, step_y: step_y, max: max)
-    print "\e[1;32m  pan: w/a/s/d, zoom: i=in, o=out, r=reset, iterations: =: more, -: less, quit: q\n"
-    print "\e[1;36m: mandelbrot -l #{left} -r #{right} -t #{top} -b #{bottom} --max #{max}\e[0m"
+    print "\e[1;32m pan: w/a/s/d h/j/k/l, zoom: i=in, o=out, r=reset, iterations: =: more, -: less, quit: q\n"
+    print "\e[1;36m -x #{mid_x} -y #{mid_y} -z #{zoom} --max #{max}\e[0m"
 
     inc = [step_x.abs, step_y.abs].max
     input = STDIN.raw &.read_char
     case input
-    when 'd'
-      left += inc; right += inc
-    when 's'
-      top -= inc; bottom -= inc
-    when 'w'
-      top += inc; bottom += inc
-    when 'a'
-      left -= inc; right -= inc
+    when 'd', 'l'
+      mid_x -= step_x * 2
+    when 's', 'j'
+      mid_y -= step_y * 2
+    when 'w', 'k'
+      mid_y += step_y * 2
+    when 'a', 'h'
+      mid_x += step_x * 2
     when 'i'
-      left += inc; right -= inc; top -= inc; bottom += inc
+      zoom *= 0.96
     when 'o'
-      left -= inc; right += inc; top += inc; bottom -= inc
+      zoom *= 1.04
+    when 'I'
+      zoom *= 0.8
+    when 'O'
+      zoom *= 1.2
     when 'r'
-      left = left0; right = right0; top = top0; bottom = bottom0
-      max = max0
+      mid_x, mid_y, zoom, max = mid_x0, mid_y0, zoom0, max0
     when '='
       max += 1
     when '-'
@@ -85,21 +100,17 @@ ensure
 end
 
 # Defaults
-left            = -2.0
-right           = 0.5
-top             = 1.0
-bottom          = -1.0
-zoom            = 1.0
+x            = 0.75
+y            = 0.0
+zoom         = 1.0
 max_iterations  = 100
 fps             = -1.0
 delay           = -1.0
 
 OptionParser.parse! do |parser|
   parser.banner = "Usage: mandelbrot [arguments]"
-  parser.on("-l LEFT", "--left=LEFT", "initial left edge") { |val| left = val.to_f }
-  parser.on("-r RIGHT", "--right=RIGHT", "initial right edge") { |val| right = val.to_f }
-  parser.on("-t TOP", "--top=TOP", "initial top edge") { |val| top = val.to_f }
-  parser.on("-b BOTTOM", "--bottom=BOTTOM", "initial bottom edge") { |val| bottom = val.to_f }
+  parser.on("-x X", "center point X") { |val| x = val.to_f }
+  parser.on("-y Y", "center point Y") { |val| y = val.to_f }
   parser.on("-z ZOOM", "--zoom=ZOOM", "initial zoom") { |val| zoom = val.to_f }
   parser.on("--max MAX", "--max=MAX", "max iterations") { |val| max_iterations = val.to_i }
   parser.on("--fps=FPS", "max FPS") { |val| delay = 1.0 / val.to_f }
@@ -109,4 +120,8 @@ OptionParser.parse! do |parser|
   end
 end
 
-zoomer(left: left, right: right, top: top, bottom: bottom, max: max_iterations)
+zoomer(mid_x: x, mid_y: y, zoom: zoom, max: max_iterations)
+# : mandelbrot -x 0.7495500935639509 -y -0.06371993917250125 -z 0.6866512314685467 --max 239
+# mandelbrot -x 0.7494718949168628 -y -0.10770718066649793 -z 2.5642564948936334e-15 --max 100
+# mandelbrot -x 0.37643238557024805 -y -0.6722880311475035 -z 1.8463281043800022e-13 --max 118  1.0
+# mandelbrot -x 0.7288780059956085 -y -0.29203160173167625 -z 1.6e-13 --max 196
